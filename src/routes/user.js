ea61a10f47d7ef2fff7ddee10e78ca253fa89e87
AjaxGroup1400 <uplayggboy@gmail.com>
2022-08-15 07:13:52 +0430

    renamed addUser middleware to signIn

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,23 +1,24 @@
 import express from "express";
 
 import {
-  addUser,
+  signUp,
   logout,
   renderSignin,
   renderSignUp,
   signIn,
 } from "../controllers/user.js";
+import { userAuth } from "../middlewares/index.js";
 
 const router = express.Router();
 
 router.get("/signup", renderSignUp);
 
-router.post("/signup", addUser);
+router.post("/signup", signUp);
 
 router.get("/signin", renderSignin);
 
 router.post("/signin", signIn);
 
-router.post("/logout", logout);
+router.post("/logout", userAuth, logout);
 
 export default router;
