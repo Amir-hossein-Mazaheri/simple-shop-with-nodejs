b3a06a5c9959f8087a6f692a2643aa5fcf44a68b
AjaxGroup1400 <uplayggboy@gmail.com>
2022-08-15 23:47:15 +0430

    added some logs for better debuging

--- src/middlewares/setLocals.js
+++ src/middlewares/setLocals.js
@@ -4,13 +4,19 @@ export const setLocals = (req, res, next) => {
 
   // to prevent passing these into unwanted pages
   const err = req.flash("error")[0];
+  const errorContents = req.flash("errorContents");
+  const errorId = req.flash("errorId");
   /*
   connect-flash package can't store Boolean values instead stored "false" and then convert
   it to Boolean
   */
   res.locals.error = (err && JSON.parse(err)) || false;
-  res.locals.errorContents = req.flash("errorContents") || [];
-  res.locals.errorId = req.flash("errorId") || "";
+  res.locals.errorContents = errorContents || [];
+  res.locals.errorId = errorId || "";
+
+  console.log("error is: ", err);
+  console.log("error contents is: ", errorContents);
+  console.log("error id is: ", errorId);
 
   next();
 };
